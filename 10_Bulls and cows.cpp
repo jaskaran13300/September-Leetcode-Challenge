1282e8e6a3dac1d725cbbcf844955b74ae785ee9
Jaskaran Batra <jskrnbtr@gmail.com>
2020-09-11 00:06:08 +0530

    Update 10_Bulls and cows.cpp
    You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
    
    Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
    
    Please note that both secret number and friend's guess may contain duplicate digits.
    
    Example 1:
    
    Input: secret = "1807", guess = "7810"
    
    Output: "1A3B"
    
    Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
    Example 2:
    
    Input: secret = "1123", guess = "0111"
    
    Output: "1A1B"
    
    Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
    Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.

--- 10_Bulls and cows.cpp
+++ 10_Bulls and cows.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    int pairs(string str1, string str2, int size){
+        int f1[11] = { 0 };
+        int f2[11] = { 0 };
+        int i, c = 0;
+		
+        for (i = 0; i < size; i++)
+          f1[str1[i] - '0']++;
+        
+        for (i = 0; i < size; i++)
+          f2[str2[i] - '0']++;
+        
+        for (i = 0; i < 11; i++)
+          c += (min(f1[i], f2[i]));
+        
+        return c;
+    }
+    string getHint(string secret, string guess) {
+        int a=0,b,n = secret.size();
+        b=pairs(secret,guess,n);
+        
+        for(int i=0;i<n;i++) 
+            if(secret[i]==guess[i])
+                a++;
+        string res=to_string(a)+'A'+to_string(b-a)+'B'; 
+        return res;
+    }
+};
