abf7f050c415bfb936b1ab0ecd763407892308f1
Jaskaran Batra <jskrnbtr@gmail.com>
2020-09-11 23:28:12 +0530

    Create 11_Maximum Product Subarray.cpp
    Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
    
    Example 1:
    
    Input: [2,3,-2,4]
    Output: 6
    Explanation: [2,3] has the largest product 6.
    Example 2:
    
    Input: [-2,0,-1]
    Output: 0
    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.

---
+++ 11_Maximum Product Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int maxProduct(vector<int>& nums) {
+        if(nums.size()==0){
+            return 0;
+        }
+        int ans=nums[0],maxi=nums[0],mini=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<0){
+                swap(maxi,mini);
+            }
+            maxi=max(maxi*nums[i],nums[i]);
+            mini=min(mini*nums[i],nums[i]);
+            ans=max(ans,maxi);
+        }
+        return ans;
+    }
+};
